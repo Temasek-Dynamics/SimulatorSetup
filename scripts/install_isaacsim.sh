036ecbec6ec42a0fa261880922a1f711e442ac93
YichaoGao <116415121+CarlsonGYC@users.noreply.github.com>
2025-02-05 15:17:20 +0800

    Fast installation scripts for Isaac sim and Pegasus Simulator (#8)
    * Update to solve conflicts in config file
    
    * Add scripts for fast installation of Isaac Sim and Pegasus Simulator
    
    * chore: update environment setup per review feedback

--- scripts/install_isaacsim.sh
+++ scripts/install_isaacsim.sh
@@ -5,6 +5,7 @@ set -e
 ISAACSIM_PATH="${HOME}/.local/share/ov/pkg/isaac-sim-4.2.0/"
 mkdir -p "$ISAACSIM_PATH"
 ISAACSIM="$ISAACSIM_PATH/isaac-sim.sh"
+URL="https://download.isaacsim.omniverse.nvidia.com/isaac-sim-standalone%404.2.0-rc.18%2Brelease.16044.3b2ed111.gl.linux-x86_64.release.zip"
 
 # Define the zip file name
 ISAACSIM_ZIP="$ISAACSIM_PATH/isaac-sim.zip"
@@ -16,9 +17,9 @@ echo -e "\e[1;32m=============================================\e[0m"
 # Download Isaac Sim
 if [ ! -f "$ISAACSIM" ]; then
     echo -e "\e[1;34m🔄 [INFO] Downloading Isaac Sim...\e[0m"
-    wget -O "$ISAACSIM_ZIP" https://download.isaacsim.omniverse.nvidia.com/isaac-sim-standalone%404.2.0-rc.18%2Brelease.16044.3b2ed111.gl.linux-x86_64.release.zip
-    unzip "$ISAACSIM_ZIP" -d "$ISAACSIM_PATH"
-    chmod +x "$ISAACSIM"
+    wget -O $ISAACSIM_ZIP $URL
+    unzip $ISAACSIM_ZIP -d $ISAACSIM_PATH
+    chmod +x $ISAACSIM
     echo -e "\e[1;32m✅ [INFO] Isaac Sim downloaded and made executable at $ISAACSIM_PATH\e[0m"
 else
     echo -e "\e[1;32m✅ [INFO] Isaac Sim already exists at $ISAACSIM_PATH, skipping download.\e[0m"
